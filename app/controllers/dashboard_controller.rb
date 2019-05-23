628a20f6a67ded60560b23714155fcee19536cba
AlinaMaican <alinutzamaican@yahoo.com>
2019-05-23 09:31:29 +0300

    some kind of functionality for dashboard

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -2,5 +2,8 @@ class DashboardController < ApplicationController
   before_action :authenticate_user!
 
   def index
+    @expenses=Expense.all
+    @reports=Report.all
+    @tags=Tag.all
   end
 end
