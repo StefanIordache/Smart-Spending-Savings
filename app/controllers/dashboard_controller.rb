7bd57740ed3e5b5e02c37909da3bbed442d2330f
StefanIordache <iordachestefan1@gmail.com>
2019-05-23 11:15:59 +0300

    Doamne ajuta

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -2,8 +2,11 @@ class DashboardController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    @expenses=Expense.all
-    @reports=Report.all
-    @tags=Tag.all
+    if current_user
+      @expenses = Expense.where(user_id: current_user.id)
+      @reports = Report.where(user_id: current_user.id)
+      @tags=Tag.where(user_id: current_user.id)
+    end
   end
+
 end
